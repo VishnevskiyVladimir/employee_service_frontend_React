e9470af682105e78b85b6c8364cb7cd0c2a47566
vlad <vishnevskiy.vladimir.mbox@gmail.com>
2021-02-06 23:28:07 +0300

    "create table for employees and EmployeeService.js which parse employee from rest api"

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import EmployeeService from "../services/EmployeeService";
+
+class ListEmployeeComponent extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees:[]
+        }
+    }
+
+    componentDidMount() {
+        EmployeeService
+            .getEmployees()
+            .then((res) => {
+                this.setState({employees: res.data});
+            });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Employees List</h2>
+                <div className="raw">
+                    <table className="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.employees.map(
+                                employee =>
+                                    <tr key={employee.id}>
+                                        <td>{employee.firstName}</td>
+                                        <td>{employee.lastName}</td>
+                                        <td>{employee.emailId}</td>
+
+                                    </tr>
+                            )
+                        }
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default ListEmployeeComponent;
