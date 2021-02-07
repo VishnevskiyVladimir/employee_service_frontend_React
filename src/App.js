924069e03feff9e6cd75b64b8325ea934fd94b3d
vlad <vishnevskiy.vladimir.mbox@gmail.com>
2021-02-07 22:24:53 +0300

    add footer,header, SaveEmployee page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-
 import './App.css';
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+
+
 function App() {
-  return (
-    <div className="container">
-      <ListEmployeeComponent/>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                    <HeaderComponent/>
+                    <div className="container">
+                        <Switch>
+                            <Route path = "/" exact component={ListEmployeeComponent}></Route>
+                            <Route path = "/employees" component={ListEmployeeComponent}></Route>
+                            <Route path = "/add-employee" component={CreateEmployeeComponent}></Route>
+                        </Switch>
+                    </div>
+                    <FooterComponent/>
+            </Router>
+
+        </div>
+    );
 }
 
 export default App;
